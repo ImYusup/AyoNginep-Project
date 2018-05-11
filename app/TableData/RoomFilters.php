3378cb85db7c7440063b239d1add4453d73e1925
fadhlimajid <fadhlimajid43@gmail.com>
2018-05-11 20:54:08 +0700

    add filtering for city and address detail

--- app/TableData/RoomFilters.php
+++ app/TableData/RoomFilters.php
@@ -11,13 +11,31 @@ class RoomFilters extends QueryFilters
                 return $this->query;
             }
         }
-        public function district($disctrict){
-            if( !empty( $disctrict ) ) {
-                return $this->query->where('district', 'like', '%' . $disctrict . '%');
+
+        public function district($district){
+            if( !empty( $district ) ) {
+                return $this->query->where('district', 'like', '%' . $district . '%');
             } else {
                 return $this->query;
             }
         }
+
+        public function city($city){
+            if( !empty( $city ) ) {
+                return $this->query->where('city', 'like', '%' . $city . '%');
+            } else {
+                return $this->query;
+            }
+        }
+
+        public function address_detail($address_detail){
+            if( !empty( $address_detail ) ) {
+                return $this->query->where('address_detail', 'like', '%' . $address_detail . '%');
+            } else {
+                return $this->query;
+            }
+        }
+        
         public function price($price){
             if( !empty( $price ) ) {
                 $to = substr($price,strpos($price,"-")+1);
@@ -33,11 +51,13 @@ class RoomFilters extends QueryFilters
                 return $this->query->orderBy('rent', 'asc');
             }
         }
+
         public function keyword($keyword){
             if($keyword){
                 return $this->query
                 ->where('name', 'LIKE', '%' . $keyword . '%')
                 ->orWhere('district', 'LIKE', '%' . $keyword . '%')
+                ->orWhere('city', 'LIKE', '%' . $keyword . '%')
                 ->orWhere('address_detail', 'LIKE', '%' . $keyword . '%')
                 ->orWhereHas('categories', function($q) use ($keyword){
                     $q->where('name', 'LIKE', '%' . $keyword . '%');
@@ -47,4 +67,3 @@ class RoomFilters extends QueryFilters
             }
         }
     }
-    ?>
